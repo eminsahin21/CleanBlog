6cea4813e08f270f3bc24d8f64c0429b0828d480
Mehmet Emin Şahin <mehmeteminsahin34@gmail.com>
2024-03-08 17:41:42 +0300

    Template Engine

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const ejs = require("ejs")
 
 const app = express();
 
+//MIDDLEWARE
+app.use(express.static("public"))
+
+//TEMPLATE ENGINE
+app.set("view engine","ejs")
+
+//ROUTES
 app.get("/", (req,res) => {
-    res.send("Merhaba")
+    res.render("index")
 })
 
 app.get("/about", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("about")
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("post")
+});
+
+app.get("/post", (req, res) => {
+  res.render("post")
 });
 
 
